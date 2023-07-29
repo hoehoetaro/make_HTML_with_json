2b9954f9cbcccea78deb9c591ccc6ae6914983db
hoehoetaro <hoehoe@kcf.biglobe.ne.jp>
2023-07-29 12:15:27 +0900

    Add a change log

--- 04_make_HTML_with_json.py
+++ 04_make_HTML_with_json.py
@@ -12,12 +12,16 @@ dir_name = os.path.basename(os.getcwd())
 with open(f"{dir_name}_data.json", "r", encoding="utf-8") as json_file:
     data = json.load(json_file)
 
+# Update dataの読み込み
+with open(data['update_data']['file_path'], 'r', encoding='utf-8') as f:
+    data['update_data']['content'] = f.read().replace('\n', '<br>')
+
 # Jinja2環境の設定
 env = Environment(loader=FileSystemLoader('.'))
 template = env.get_template('templates/template.html')
 
 # テンプレートにデータを適用
-html_output = template.render(data)
+html_output = template.render(**data)
 
 # HTMLファイルとして出力、出力するファイル名を現在のフォルダ名.htmlにする
 with open(f"{dir_name}.html", "w", encoding='utf-8') as html_file:
